8a1a6c0e16eb52d95de3605940649d61cb1c44a1
glyfspace <rishipandey125@gmail.com>
2025-01-22 13:50:59 -0800

    create full set of frames

--- nodes.py
+++ nodes.py
@@ -24,7 +24,7 @@ def convert_numpy_to_tensor(numpy_image):
     
 class PackFrames:
     @classmethod
-    def INPUT_TYPES(s):
+    def INPUT_TYPES(cls):
         return {
             "required": {
                 "images": ("IMAGE", {"tooltip": "Input images to be packed"}),
@@ -37,8 +37,8 @@ class PackFrames:
                 "min_frame_height": ("INT", {"default": 512, "min": 512, "max": 5120, "step": 512}),
             },
         }
-    OUTPUT_NODE = True
-    RETURN_TYPES = ("IMAGE", "INT", "INT",)
+
+    RETURN_TYPES = ("IMAGE", "INT", "INT")
     RETURN_NAMES = ("sheets", "frame_width", "frame_height")
     FUNCTION = "pack_frames"
     CATEGORY = "FramePack"
@@ -46,7 +46,6 @@ class PackFrames:
     def pack_frames(self, images, num_sheets, sheet_width, sheet_height, min_frame_width, min_frame_height):
         images_np = convert_tensor_to_numpy(images)
 
-        # Calculate frames and dimensions
         total_frames = images_np.shape[0]
         frames_per_sheet = math.ceil(total_frames / num_sheets)
         side_count = math.ceil(math.sqrt(frames_per_sheet))
@@ -55,27 +54,22 @@ class PackFrames:
 
         packed_sheets = []
         current_frame = 0
+
         for sheet_index in range(num_sheets):
             sprite_sheet = np.zeros((sheet_height, sheet_width, 3), dtype=np.uint8)
             for row in range(side_count):
                 for col in range(side_count):
-                    if current_frame >= total_frames:
-                        break
-                    frame = images_np[current_frame]
-                    resized_frame = cv2.resize(frame, (frame_width, frame_height), interpolation=cv2.INTER_AREA)
+                    if current_frame < total_frames:
+                        frame = images_np[current_frame]
+                        resized_frame = cv2.resize(frame, (frame_width, frame_height), interpolation=cv2.INTER_AREA)
+                    else:
+                        resized_frame = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)  # Black frame
+
                     x = col * frame_width
                     y = row * frame_height
                     sprite_sheet[y:y + frame_height, x:x + frame_width] = resized_frame
                     current_frame += 1
-                if current_frame >= total_frames:
-                    break
             packed_sheets.append(sprite_sheet)
-            if current_frame >= total_frames:
-                break
 
-
-        # packed_sheets_tensor = numpy_to_tensor(packed_sheets)
         packed_sheets_tensor = [convert_numpy_to_tensor(sheet) for sheet in packed_sheets]
-
-
-        return (torch.stack(packed_sheets_tensor), frame_width, frame_height,)
+        return torch.stack(packed_sheets_tensor), frame_width, frame_height
